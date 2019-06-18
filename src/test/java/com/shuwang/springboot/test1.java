9bd7baeeed8326d96efead27234a98dde2209722
wangshu <373118970@qq.com>
2019-06-18 15:54:29 +0800

    第一次使用GitHub,心情好激动！>_< 2019-06-17 16:44

--- src/test/java/com/shuwang/springboot/test1.java
+++ src/test/java/com/shuwang/springboot/test1.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class test1 {
-    @Test
-    public void test(){
+    public static void main(String[] args) {
         for(int i=0;i<10;++i){
             System.out.println("1");
+            //1
         }
     }
 }
